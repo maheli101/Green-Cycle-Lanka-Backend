104d4ad9937414a5a5aaa27f3c45affe5365b90c
maheli101 <maheligeorge@gmail.com>
2024-06-05 15:05:25 +0530

    order controller

--- Controller/OrderController.js
+++ Controller/OrderController.js
@@ -29,7 +29,7 @@ const postOrder = async (req, res) => {
 
 const getOrders = async (req, res) => {
     try {
-        const orders = await Order.find().populate('user_id', 'name email');
+        const orders = await Order.find().populate('user_id', 'name email contactNumber ');
         res.status(200).json(orders);
     } catch (error) {
         res.status(500).json({ message: error.message });
