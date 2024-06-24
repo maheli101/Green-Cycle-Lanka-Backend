6f4e88dbccb5420be5cd5b3bffe391f2d934cfdc
lakvindu2002 <lakvindusampath125@gmail.com>
2024-06-24 22:13:53 +0530

    done and dust

--- Controller/UserController.js
+++ Controller/UserController.js
@@ -1,5 +1,5 @@
 const User = require("../Models/UserModel.js");
-
+const bcrypt = require('bcrypt');
 
 const getUser = async (req, res) => {
   try {
@@ -12,20 +12,33 @@ const getUser = async (req, res) => {
 
 const postUser = async (req, res) => {
   try {
+    const { name, email, contactNumber, NIC, password, type, profilePicture } = req.body;
+    
+    // Log the request body (for debugging)
     console.log(req.body);
-    
-     await User.create(req.body);
-     res.status(200).json({ message: "User Created Successful" });
-    
 
-  
+    // Hash the password
+    const saltRounds = 10; // Adjust the number of salt rounds as needed
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // Create a new user with the hashed password
+    const newUser = {
+      name,
+      email,
+      contactNumber,
+      NIC,
+      password: hashedPassword,
+      type,
+      profilePicture
+    };
+
+    await User.create(newUser);
+    res.status(200).json({ message: "User Created Successfully" });
   } catch (error) {
     console.log(error.message);
     res.status(500).json({ message: error.message });
-    // console.log("can't create a user use unique e-mail and")
   }
 };
-
 const getCurrentUser = async (req, res) => {
   try {
     const userId = req.params.id; // Assuming the user ID is stored in the req.user object
